d113acc1ab99738312e42539d9778b6e8cd8ae3b
Rinkesh <rinkeshujjwal16@gmail.com>
2022-11-30 10:49:11 +0530

    problem has been fixed

--- prime.js
+++ prime.js
@@ -1,5 +1,28 @@
-let a=19;
-let count=0;
-for (let i=0;i<19;i++){
-	if (a%i==0){
-		count++;}
+let num = 13
+let prime = 'Yes';
+if(num<=1){
+	console.log('No')
+}
+else{
+	for(let i = 2;i<=Math.sqrt(num);i++){
+		if(num%i===0){
+			prime = 'No'
+			break;
+		}
+	}
+	console.log(prime)
+}
+"_____________________________________________________"
+let str = "racecar"
+let palindrome = true
+let start = 0
+let end = str.length-1
+while(start<=end){
+	if(str[start] !== str[end]){
+		palindrome = false;
+		break
+		}
+		start++
+		end--
+}
+console.log(palindrome)
